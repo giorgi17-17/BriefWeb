074f8b4239f87d5efa2c97edda59a18d714b846c
Giorgi <gfifia15@gmail.com>
2025-05-19 15:55:56 +0400

    gemini api key name changed

--- server/config/gemini.js
+++ server/config/gemini.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const geminiAI = new GoogleGenAI({
-  apiKey: process.env.VITE_GEMINI_API_KEY,
+  apiKey: process.env.GEMINI_API_KEY,
 });
 
 export const geminiModel = "gemini-2.0-flash-lite";
