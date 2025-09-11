83154338548ce8c2feb53aa80779f2e71087f3bf
davidForDev <dato.takidze@geolab.edu.ge>
2025-09-11 18:41:13 +0400

    Update/gemini-config

--- server/config/gemini.js
+++ server/config/gemini.js
@@ -1,10 +1,23 @@
-import { GoogleGenAI } from "@google/genai";
+import {GoogleGenAI} from '@posthog/ai'
+import { PostHog } from 'posthog-node';
 import dotenv from "dotenv";
+
 dotenv.config();
 
+const posthog = new PostHog(
+  process.env.POSTHOG_API_KEY,
+  { 
+    host: process.env.POSTHOG_HOST || 'https://app.posthog.com',
+    flushAt: 20,
+    flushInterval: 10000,
+  }
+);
+
 export const geminiAI = new GoogleGenAI({
-  apiKey: "AIzaSyClGRl_js0aJwjvebgJC3NTsU36ckbIovA"
+  apiKey: "AIzaSyClGRl_js0aJwjvebgJC3NTsU36ckbIovA",
+  posthog: posthog,
 });
+
 export const geminiModel = "gemini-2.0-flash-lite";
 export const geminiModelFlashCard = "gemini-2.0-flash-lite";
 export const geminiModelBrief = "gemini-2.5-flash-lite";
