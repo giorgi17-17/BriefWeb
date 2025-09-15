dc7398db7b89cfbcf0ce334a994ab1530beeb22a
Giorgi <gfifia15@gmail.com>
2025-09-15 20:49:11 +0400

    meta pixel added

--- server/config/gemini.js
+++ server/config/gemini.js
@@ -5,7 +5,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const posthog = new PostHog(
-  process.env.POSTHOG_API_KEY,
+  process.env.POSTHOG_KEY,
   { 
     host: process.env.POSTHOG_HOST || 'https://app.posthog.com',
     flushAt: 20,
